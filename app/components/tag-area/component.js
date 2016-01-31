53107d3338f3ea624bed78f00547f829b587aa4b
Paul <pmc.consult@gmail.com>
2016-01-30 20:23:01 -0600

    Determine selection start value on focus and key ups

--- app/components/tag-area/component.js
+++ app/components/tag-area/component.js
@@ -1,7 +1,24 @@
 import Ember from 'ember';
 
+function tagArea() {
+	return Ember.$('.tag-area');
+}
+
+function selStart() {
+	return tagArea().prop('selectionStart');
+}
+
 export default Ember.Component.extend({
 	rowCount: 15,
 	colCount: 80,
-	screenText: null
+	screenText: '',
+	cursorIndex: -1,
+	actions: {
+		keyUp() {
+			this.set('cursorIndex', selStart());
+		},
+		gotFocus() {
+			this.set('cursorIndex', selStart());
+		}
+	}
 });
