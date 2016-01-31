945334eaf975c7aa0244f160eb6ef7174bcca629
Paul <pmc.consult@gmail.com>
2016-01-30 18:10:17 -0600

    Add a tag route that holds a tag-area component

---
+++ tests/unit/tag/route-test.js
@@ -0,0 +1,9 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:tag', 'Unit | Route | tag', {
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
